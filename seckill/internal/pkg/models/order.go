d920cd2d6c70fdf0fb109c01d431badd514b51d0
SKDG042 <3212580740@qq.com>
2025-05-04 00:42:07 +0800

    feat: 定义orderStatus常量并修改结构体以方便操作

--- seckill/internal/pkg/models/order.go
+++ seckill/internal/pkg/models/order.go
@@ -1,7 +1,18 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
+)
+
+const (
+	// 订单状态常量
+	StatusPending   = 0 // 订单创建中/订单未支付
+	StatusCreated   = 1 // 订单创建成功
+	StatusPaid      = 2 // 订单已支付
+	StatusFailed    = 3 // 订单创建失败
+	StatusCancelled = 4 // 订单已取消
 )
 
 type Order struct{
@@ -11,6 +22,9 @@ type Order struct{
 	ActivityID 	uint 	`gorm:"not null;index"`
 	Product 	Product `gorm:"foreignKey:ProductID"`
 	Activity 	Activity`gorm:"foreignKey:ActivityID"`
+	OrderSn 	string 	`gorm:"not null;unique"`
+	Amount 		float64 `gorm:"type:decimal(10,2); not null"`
+	CreateTime 	time.Time `gorm:"not null"`
 	Price     	float64 `gorm:"type:decimal(10,2); not null"`
 	Quantity 	int 	`gorm:"not null"`
 	Status 		int 	`gorm:"not null;default:1"` // 1:待支付，2:已支付，3:已取消
