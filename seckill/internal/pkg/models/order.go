f2675bf817b10abd884bd8cc74680ae3fd211f24
SKDG042 <3212580740@qq.com>
2025-05-03 14:19:12 +0800

    fix: 修改models

--- seckill/internal/pkg/models/order.go
+++ seckill/internal/pkg/models/order.go
@@ -6,9 +6,12 @@ import (
 
 type Order struct{
 	gorm.Model
-	UserID 		uint `gorm:"not null;index"`
-	ProductID 	uint `gorm:"not null;index"`
+	UserID 		uint 	`gorm:"not null;index"`
+	ProductID 	uint 	`gorm:"not null;index"`
+	ActivityID 	uint 	`gorm:"not null;index"`
+	Product 	Product `gorm:"foreignKey:ProductID"`
+	Activity 	Activity`gorm:"foreignKey:ActivityID"`
 	Price     	float64 `gorm:"type:decimal(10,2); not null"`
-	Quantity 	int `gorm:"not null"`
-	Status 		int `gorm:"not null;default:1"` // 1:待支付，2:已支付，3:已取消
+	Quantity 	int 	`gorm:"not null"`
+	Status 		int 	`gorm:"not null;default:1"` // 1:待支付，2:已支付，3:已取消
 }
